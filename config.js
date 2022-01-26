2fa65b0ba92b8b6d178855da2593ab18120b2498
Trịnh Đức Đại <dai.trinhduc@ncc.asia>
2022-01-26 19:15:54 +0700

    update config

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
   //IMPORTANT: If you need help with the installation of KOMU, you can join our support server here: https://komu.vn/discord
-  prefix: '*',
+  prefix: "*",
   // Your discord bot token. https://discord.com/developpers/bots
-  token: 'OTM1NzU1Nzg3MTgyMTc0MjM4.YfDQfQ.MglB8Xj8rxkRPUJuxhZRZK_OYkI',
+  token: "",
   // Your ID
   // Your name/tag
-  owners: ['KOMU#1769'],
+  owners: ["KOMU#0139"],
   //the default bot language. fr or en
-  defaultLanguage: 'en',
+  defaultLanguage: "en",
   // If dev mod is enabled
   devMode: false,
   // The server where you test the commands
-  devServer: '935755787182174238',
+  devServer: "782661233622515772",
   // If you want to log every command,event etc. Usefull for debuging
   logAll: false,
   // If you want to test your configuration before starting the bot
@@ -20,11 +20,11 @@ module.exports = {
   shards: 1,
   //Database
   database: {
-    // The url of your mongodb database. Check mongodb.org
-    MongoURL: 'mongodb://localhost:27017/vinhbot',
-    // If you want to cache the database. For big bots
-    cached: true,
-    delay: 300000 * 4,
-    Options: { autoIndex: false },
-  },
-};
+      // The url of your mongodb database. Check mongodb.org
+      MongoURL: "mongodb://localhost:27017/komubot",
+      // If you want to cache the database. For big bots
+      cached: true,
+      delay: 300000 * 4,
+      Options: { autoIndex: false }
+  }
+}
