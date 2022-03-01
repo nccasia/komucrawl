d8ad8cff104a8bf5fea547eb42f77a522bcf572a
le duc anh <anh.leduc1@ncc.asia>
2022-03-01 10:21:15 +0700

    add joinCall

--- events/discord/voiceStateUpdate.js
+++ events/discord/voiceStateUpdate.js
@@ -1,8 +1,67 @@
 const checkCameraData = require('../../models/checkCameraData');
-
+const joinCallData = require('../../models/joinCall')
 module.exports = {
   async execute(oldState, newState) {
     try {
+
+      let countMember = newState.channel?.members.size ? newState.channel.members.size : oldState.channel.members.size
+      const allMember = newState.channel?.members ? newState.channel?.members : oldState.channel.members
+      
+       const addJoinCall =async (channelId, userid,status) => {
+        const newjoinCall = new joinCallData({
+          channelId,
+          userid ,
+          status ,
+        })
+        await newjoinCall.save()
+      }
+      const checkDbIsJoinning = async (channelId, userid,status) => {
+        let result = false
+        const joindb = await joinCallData.findOne({
+          channelId,
+          userid ,
+          status ,
+        })
+        if(joindb) result = true
+        return result
+      }
+      const updateJoiningDb = async (channelId, userid, status) => {
+        await joinCallData.updateOne({channelId,userid,status : "joining"} , {
+          status ,
+          end_time : Date.now()
+        })
+      }        
+      
+        // !newState.channelId => leave room
+        // !oldState.channelId => join room
+        // one member leave when totals member = 2
+        if(countMember === 1 && !newState.channelId) {
+          await updateJoiningDb(oldState.channelId,oldState.id, "finish")
+          const userid = oldState.channel.members.map(item=>item.user.id)[0]
+          await updateJoiningDb(oldState.channelId, userid, "finish")
+        }
+        // one member joinning hence total member = 2
+        if(countMember === 2 &&  !oldState.channelId) {
+           const userid = newState.channel.members.map(item=>item.user.id) 
+            await addJoinCall(newState.channelId,userid[0], "joining") 
+            await addJoinCall(newState.channelId,userid[1], "joining") 
+        }
+        if(countMember === 2 && !newState.channelId) {
+          await updateJoiningDb(oldState.channelId, oldState.id, "finish")
+        }
+        if(countMember > 2 ) {
+            if(!oldState.channelId){
+            await addJoinCall(
+              newState.channelId,
+              newState.id,
+              "joining",
+            )
+          }
+          // check leave joinning
+          if(!newState.channelId) {
+          await updateJoiningDb(oldState.channelId, oldState.id, "finish")
+          }
+        }
       if (oldState.selfVideo === false && newState.selfVideo === true) {
         await new checkCameraData({
           userId: newState.id,
